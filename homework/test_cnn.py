f061703fcb83df80f013731284a53aac0922fff0
Aadityavardhan Narayan <anarayan@connect.ust.hk>
2023-04-01 21:27:03 +0800

    Q3 Test loop fixed

--- homework/test_cnn.py
+++ homework/test_cnn.py
@@ -10,7 +10,7 @@ def test(args):
     model = CNNClassifier()
     model.eval()
     batch_size = 64
-    model.load_state_dict(torch.load('./model_20230401_063051_10', torch.device('cpu')))
+    model.load_state_dict(torch.load(args.log_dir, torch.device('cpu')))
     train_data = load_data('drive-download-20230329T090612Z-001/train_subset', 2, batch_size)
     valid_data = load_data('drive-download-20230329T090612Z-001/validation_subset', 2, batch_size)
 
@@ -26,31 +26,50 @@ def test(args):
     train_cm = ConfusionMatrix(6)
     valid_cm = ConfusionMatrix(6)
 
-    for i, data in enumerate(train_data):
-        inputs, labels = data
-        # print(inputs.shape)
-        # print(labels.shape)
-        inputs, labels = inputs.to(device), labels.to(device)
-        outputs = model(inputs)
-        outputs = outputs.max(1)[0]
+    with torch.no_grad():
+        for i, data in enumerate(train_data):
+            inputs, labels = data
+            # print(inputs.shape)
+            # print(labels.shape)
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            outputs = outputs.max(1)[1]
 
 
-        # print(outputs.shape)
+            # print(outputs.shape)
 
-        train_cm.add(outputs, labels)
-    print("Clear 1")
+            train_cm.add(outputs, labels)
+        print("Clear 1")
 
-    for i, data in enumerate(valid_data):
-        inputs, labels = data
-        # print(labels.shape)
-        inputs, labels = inputs.to(device), labels.to(device)
-        outputs = model(inputs)
-        outputs = outputs.max(1)[0]
+        for i, data in enumerate(valid_data):
+            inputs, labels = data
+            # print(labels.shape)
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            outputs = outputs.max(1)[1]
 
-        # print(outputs.shape)
+            # print(outputs.shape)
 
-        valid_cm.add(outputs, labels)
-    print("Clear all")
+            valid_cm.add(outputs, labels)
+
+        print("Clear all")
+
+    # Get evaluation metrics
+    print("Training: ")
+    print(train_cm.global_accuracy)
+    print(train_cm.class_accuracy)
+    print(train_cm.average_accuracy)
+    print(train_cm.iou)
+    print(train_cm.class_iou)
+
+    print("Validation: ")
+    print(valid_cm.global_accuracy)
+    print(valid_cm.class_accuracy)
+    print(valid_cm.average_accuracy)
+    print(valid_cm.iou)
+    print(valid_cm.class_iou)
+
+    print("End")
 
     return train_cm.average_accuracy, valid_cm.average_accuracy
 
@@ -64,5 +83,5 @@ if __name__ == '__main__':
     # Put custom arguments here
 
     args = parser.parse_args()
-    print(test(args))
+    test(args)
 
