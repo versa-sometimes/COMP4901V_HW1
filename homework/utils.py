3f6348f0b0ba3cc5a0d2562c617eadc09d1654fc
Aadityavardhan Narayan <anarayan@connect.ust.hk>
2023-04-01 21:03:20 +0800

    Q3 Fixed Transform

--- homework/utils.py
+++ homework/utils.py
@@ -28,11 +28,11 @@ class VehicleClassificationDataset(Dataset):
         #     for file in os.listdir('./'+dataset_path+'/train_subset'+type):
         tf = transforms.Compose([
             transforms.Resize((224, 224)),
-            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
             transforms.RandomHorizontalFlip(),
             transforms.RandomRotation(10),
             transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
             transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
         ])
         for path in glob(dataset_path+'/*'):
             for img in glob(path+'/*'):
