ee99d34d523c25587c42b67d3bac3fa07af72b87
Aadityavardhan Narayan <anarayan@connect.ust.hk>
2023-04-04 00:57:23 +0800

    Q4 fixing unknown bug

--- homework/models.py
+++ homework/models.py
@@ -70,7 +70,7 @@ class FCN_ST(torch.nn.Module):
         self.conv2 = nn.Conv2d(in_channels=512+256, out_channels=256, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(in_channels=256+128, out_channels=128, kernel_size=3, stride=1, padding=1)
         self.conv4 = nn.Conv2d(in_channels=64+64, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.conv5 = nn.Conv2d(in_channels=32, out_channels=9, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(in_channels=32, out_channels=19, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x):
         """
