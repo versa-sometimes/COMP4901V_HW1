12426c58a580d64bc6c254cc5a3cf50dc96f894c
Aadityavardhan Narayan <anarayan@connect.ust.hk>
2023-04-04 00:46:38 +0800

    Q4 size fix

--- homework/models.py
+++ homework/models.py
@@ -63,10 +63,14 @@ class FCN_ST(torch.nn.Module):
         self.up_conv4 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1, output_padding=1)
         self.bn4 = nn.BatchNorm2d(64)
 
+        self.up_conv5 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.bn5 = nn.BatchNorm2d(64)
+
         self.conv1 = nn.Conv2d(in_channels=1024+512, out_channels=512, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(in_channels=512+256, out_channels=256, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(in_channels=256+128, out_channels=128, kernel_size=3, stride=1, padding=1)
-        self.conv4 = nn.Conv2d(in_channels=64+64, out_channels=19, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=64+64, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(in_channels=32, out_channels=9, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x):
         """
@@ -118,6 +122,11 @@ class FCN_ST(torch.nn.Module):
         x = torch.cat([x, x_skip4], dim=1)
 
         x = self.conv4(x)
+
+        x = self.up_conv5(x)
+        x = self.bn4(x)
+
+        x = self.conv5(x)
 
         return x
 
