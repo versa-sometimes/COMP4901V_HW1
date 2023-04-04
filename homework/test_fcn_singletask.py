d0b50bebf8a843c194e36955be6013862d9f1201
Aadityavardhan Narayan <anarayan@connect.ust.hk>
2023-04-04 15:59:22 +0800

    Q4 corrected commit

--- homework/test_fcn_singletask.py
+++ homework/test_fcn_singletask.py
@@ -15,8 +15,85 @@ def test(args):
     Hint: use the ConfusionMatrix for you to calculate accuracy, mIoU for the segmentation task
      
     """
+    model = FCN_ST()
+    model.eval()
+    batch_size = 64
+    model.load_state_dict(torch.load(args.log_dir, torch.device('cpu')))
+    train_data = load_dense_data('drive-download-20230401T115945Z-001/train', 2, batch_size)
+    valid_data = load_dense_data('drive-download-20230401T115945Z-001/val', 2, batch_size)
+    test_data = load_dense_data('drive-download-20230401T115945Z-001/test', 2, batch_size)
 
-    return accuracy, mIoU
+    if torch.cuda.is_available():
+        device = torch.device("cuda")    # select GPU device
+        model = model.to(device)   
+
+    train_cm = ConfusionMatrix(19)
+    valid_cm = ConfusionMatrix(19)
+    test_cm = ConfusionMatrix(19)
+
+    with torch.no_grad():
+        for i, data in enumerate(train_data):
+            inputs, labels, depth = data
+            if torch.cuda.is_available():
+                inputs, labels, depth = inputs.to(device), labels.to(device), depth.to(device)
+            outputs = model(inputs)
+            outputs = outputs.max(1)[1]
+            # print(outputs.shape)
+
+            train_cm.add(outputs, labels)
+        print("Clear 1")
+
+        for i, data in enumerate(valid_data):
+            inputs, labels, depth = data
+            if torch.cuda.is_available():
+                inputs, labels, depth = inputs.to(device), labels.to(device), depth.to(device)
+            outputs = model(inputs)
+            outputs = outputs.max(1)[1]
+
+            # print(outputs.shape)
+
+            valid_cm.add(outputs, labels)
+        
+        print("Clear 2")
+
+        for i, data in enumerate(test_data):
+            inputs, labels, depth = data
+            if torch.cuda.is_available():
+                inputs, labels, depth = inputs.to(device), labels.to(device), depth.to(device)
+            outputs = model(inputs)
+            outputs = outputs.max(1)[1]
+
+            # print(outputs.shape)
+
+            test_cm.add(outputs, labels)
+
+        print("Clear all")
+
+    print("Training: ")
+    print(train_cm.global_accuracy)
+    print(train_cm.class_accuracy)
+    print(train_cm.average_accuracy)
+    print(train_cm.iou)
+    print(train_cm.class_iou)
+
+    print("Validation: ")
+    print(valid_cm.global_accuracy)
+    print(valid_cm.class_accuracy)
+    print(valid_cm.average_accuracy)
+    print(valid_cm.iou)
+    print(valid_cm.class_iou)
+
+    print("Testing: ")
+    print(test_cm.global_accuracy)
+    print(test_cm.class_accuracy)
+    print(test_cm.average_accuracy)
+    print(test_cm.iou)
+    print(test_cm.class_iou)
+
+    print("End")
+        
+
+    return train_cm.average_accuracy, valid_cm.average_accuracy, test_cm.average_accuracy
 
 
 if __name__ == '__main__':
