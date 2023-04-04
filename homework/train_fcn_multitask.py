3cab63f40250a2076321df9372f62d6c3194ce90
Aadityavardhan Narayan <anarayan@connect.ust.hk>
2023-04-05 00:52:26 +0800

    Q4 lighter work

--- homework/train_fcn_multitask.py
+++ homework/train_fcn_multitask.py
@@ -38,8 +38,8 @@ def train(args):
 
     loss = MultiTaskLoss(weights)
 
-    train_data = load_dense_data('drive-download-20230401T115945Z-001/train', num_workers=4, batch_size=64, transform=train_tf)
-    valid_data = load_dense_data('drive-download-20230401T115945Z-001/val', num_workers=4, batch_size=64, transform=valid_tf)
+    train_data = load_dense_data('drive-download-20230401T115945Z-001/train', num_workers=2, batch_size=32, transform=train_tf)
+    valid_data = load_dense_data('drive-download-20230401T115945Z-001/val', num_workers=2, batch_size=32, transform=valid_tf)
 
     """
     Your code here
