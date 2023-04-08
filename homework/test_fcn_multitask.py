ab3d48a934bbc6234d6a220c999ff3fa92de0155
Aadityavardhan Narayan <anarayan@connect.ust.hk>
2023-04-08 19:39:01 +0800

    Q4 bugfix attempt

--- homework/test_fcn_multitask.py
+++ homework/test_fcn_multitask.py
@@ -158,6 +158,6 @@ if __name__ == '__main__':
     test(args)
 
     test_data_vis = load_dense_data('drive-download-20230401T115945Z-001/test', 2, batch_size=6)
-    inputs, labels, depth = test_data_vis[0]
+    inputs, labels, depth = list(test_data_vis)[0]
 
     DenseVisualization(inputs, depth, labels).__visualizeitem__()
